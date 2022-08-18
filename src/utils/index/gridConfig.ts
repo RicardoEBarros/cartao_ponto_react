5b96755778581d47f4d3d3f54a969747499ddd34
Ricardo Barros <169519@upf.br>
2022-08-18 14:11:49 -0300

    refactor: adjust columns width from index data grid

--- src/utils/index/gridConfig.ts
+++ src/utils/index/gridConfig.ts
@@ -2,12 +2,12 @@ import { GridColDef } from '@mui/x-data-grid'
 
 const columns = (): GridColDef[] => {
     return [
-        { field: 'id', headerName: '#' },
-        { field: 'date_time', headerName: 'Data/Hora' },
-        { field: 'start_office', headerName: 'Início Exp.' },
-        { field: 'start_interval', headerName: 'Início Intervalo' },
-        { field: 'end_interval', headerName: 'Fim Intervalo' },
-        { field: 'end_office', headerName: 'Fim Expediente' },
+        { field: 'id', headerName: '#', width: 30 },
+        { field: 'date_time', headerName: 'Data/Hora', width: 150 },
+        { field: 'start_office', headerName: 'Início Exp.', width: 120 },
+        { field: 'start_interval', headerName: 'Início Intervalo', width: 150 },
+        { field: 'end_interval', headerName: 'Fim Intervalo', width: 150 },
+        { field: 'end_office', headerName: 'Fim Expediente', width: 150 },
     ]
 }
 
