d0b85fb65899e42942a03badb3fda6bacfac8315
Ricardo Barros <169519@upf.br>
2022-08-18 11:54:46 -0300

    feat: added search time card input component and button to add new time card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from 'react'
+import { Box, Paper } from '@mui/material'
+import './css/style.css'
+import NewTimeCard from './components/NewTimeCard.tsx'
+import FilterData from './components/FilterData.tsx';
+
 function App() {
   return (
     <div className="App">
-    
+      <Box>  
+        <Paper 
+          elevation={3} 
+          sx={{
+            width: "60%",
+            margin: "20px auto",
+            height: "500px",
+            padding: "10px"
+          }}
+        >
+          <div className='actions'>
+            <NewTimeCard />
+            <FilterData />
+          </div>
+        </Paper>        
+      </Box>
     </div>
   );
 }
