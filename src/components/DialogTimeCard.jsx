11f715bb9971a9ba05142e099ff53a9f1138136f
Ricardo Barros <169519@upf.br>
2022-08-19 17:16:42 -0300

    refactor: created components for some functionality

--- src/components/DialogTimeCard.jsx
+++ src/components/DialogTimeCard.jsx
@@ -1,26 +1,19 @@
 import React, { useState, useContext } from 'react'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
+import { TimeCardContext } from './Context/TimeCardContext'
 import {
     Button,
     Dialog,
     DialogContent,
     DialogActions,
     DialogTitle,
-    MenuItem,
-    TextField,
-    Icon,
+    Stack
 } from '@mui/material'
-import { TimeCardContext } from './Context/TimeCardContext'
+import TimeCardInsertionOptions from './DialogTimeCard/TimeCardInsertionOptions'
+import TimeCardGridData from './DialogTimeCard/TimeCardGridData'
 
 export default function DialogTimeCard() {
 
-    const [timeCardType, setTimeCardType] = useState('1')
-    const [time, setTime] = useState('')
     const { openDialog, handleClick } = useContext(TimeCardContext)
-
-    const handleTimeCardType = (event) => {
-        setTimeCardType(event.target.value)
-    }
 
     return (
         <Dialog
@@ -31,28 +24,8 @@ export default function DialogTimeCard() {
         >
             <DialogTitle>Novo Ponto</DialogTitle>
             <DialogContent>
-                <Select
-                    className='new-card-entry'
-                    label="Tipo"
-                    labelId='select-type'
-                    value={timeCardType}
-                    onChange={handleTimeCardType}
-                >
-                    <MenuItem value={1}>Início Expediente</MenuItem>
-                    <MenuItem value={2}>Início Intervalo</MenuItem>
-                    <MenuItem value={3}>Fim Intervalo</MenuItem>
-                    <MenuItem value={4}>Fim Expediente</MenuItem>
-                </Select>
-                <TextField
-                    className='new-card-entry'
-                    variant="outlined"
-                    value={time}
-                ></TextField>
-                <Button
-                    className='new-card-entry'
-                    variant="contained"
-                    color="primary">
-                </Button>
+                <TimeCardInsertionOptions />
+                <TimeCardGridData />
             </DialogContent>
             <DialogActions>
                 <Button variant='contained' color="error" onClick={() => handleClick(false)}>Fechar</Button>
