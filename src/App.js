148de7d9ae440f5d6af8eeb6250c719a06ff7e27
Ricardo Barros <169519@upf.br>
2022-08-22 14:42:40 -0300

    refactor: created and added data grid component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
-import { useState } from 'react'
-import { Box, Paper} from '@mui/material'
-import TimeCard from './components/TimeCard'
-import FilterData from './components/FilterData';
+import React from 'react'
+import { Box, Paper, Stack } from '@mui/material'
+import TimeCard from './pages/TimeCard'
+import FilterData from './pages/FilterData';
 import { DataGrid } from '@mui/x-data-grid'
-import { columns } from './utils/index/gridConfig'
 import './css/style.css'
+import TimeCardGridDataIndex from './pages/TimeCardGridDataIndex';
+import TimeCardDataProvider from './components/Context/TimeCardDataContext';
 
 function App() {
-
-  const [rows, setRows] = useState([])
-
   return (
-    <div className="App">
-      <Box>  
-        <Paper 
-          elevation={3} 
-          sx={{
-            width: "60%",
-            margin: "20px auto",
-            height: "500px",
-            padding: "25px"
-          }}
-        >
-          <div className='actions'>
-            <TimeCard />
-            <FilterData />
-          </div>
-          <div style={{ 
-              marginTop: "10px",
-              width: "100%",
-              height: "400px" 
-          }}>
-            <DataGrid rows={rows} columns={columns()} />
-          </div>
-        </Paper>        
-      </Box>
-    </div>
+    <TimeCardDataProvider>
+      <div className="App">
+        <Box>  
+          <Paper 
+            elevation={3} 
+            sx={{
+              width: "60%",
+              margin: "20px auto",
+              height: "500px",
+              padding: "25px"
+            }}
+          >
+            <div className='actions'>
+              <TimeCard />
+              <FilterData />
+            </div>
+            <TimeCardGridDataIndex />
+          </Paper>        
+        </Box>
+      </div>
+    </TimeCardDataProvider>    
   );
 }
 
