80a20ee1060543a8a81f444fc0259ae2767304ca
Ricardo Barros <169519@upf.br>
2022-08-22 18:46:33 -0300

    refactor: changed context param to useContext

--- src/pages/TimeCardGridDataIndex.jsx
+++ src/pages/TimeCardGridDataIndex.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { Stack } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid'
 import { TimeCardDataContext } from '../components/Context/TimeCardDataContext'
@@ -6,14 +6,14 @@ import { columns } from '../utils/index/gridConfig'
 
 export default function TimeCardGridDataIndex() {
 
-    const { rows } = useContext(TimeCardDataContext)
+    const { rowsIndex } = useContext(TimeCardDataContext)
 
     return (
         <Stack mt={2} sx={{
             width: "100%",
             height: "400px"
         }}>
-            <DataGrid rows={rows} columns={columns()} />
+            <DataGrid rows={rowsIndex} columns={columns()} />
         </Stack>
     )
 }
