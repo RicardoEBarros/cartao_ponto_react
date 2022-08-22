d9cb7506017a5449d4a77f91f245460fb3d305d8
Ricardo Barros <169519@upf.br>
2022-08-22 14:28:22 -0300

    chore: changed path from some files

--- src/components/FilterData.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { TextField } from '@mui/material'
-
-export default function FilterData() {
-    return (
-        <TextField label="Pesquisar" variant="outlined" />
-    )
-}
