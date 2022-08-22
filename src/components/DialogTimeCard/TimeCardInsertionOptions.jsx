68f8abd227f51b246e6d6a0089ba0cfd4299f6ce
Ricardo Barros <169519@upf.br>
2022-08-22 14:37:30 -0300

    feat: added formik

--- src/components/DialogTimeCard/TimeCardInsertionOptions.jsx
+++ src/components/DialogTimeCard/TimeCardInsertionOptions.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { useFormik } from 'formik'
 import {
     MenuItem,
     TextField,
@@ -7,43 +8,59 @@ import {
     Button
 } from '@mui/material'
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { TimeCardDataContext } from '../Context/TimeCardDataContext';
 
 export default function TimeCardInsertionOptions() {
 
-    const [timeCardType, setTimeCardType] = useState('1')
-    const [time, setTime] = useState('')
-    const handleTimeCardType = (event) => setTimeCardType(event.target.value)
+    const { setRow } = useContext(TimeCardDataContext)
+
+    const { values, handleChange, handleSubmit } = useFormik({
+        initialValues: {
+            type: 1,
+            time: ''
+        }
+    })
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={4}>
-                <Select
-                    label="Tipo"
-                    labelId='select-type'
-                    value={timeCardType}
-                    onChange={handleTimeCardType}
-                >
-                    <MenuItem value={1}>Início Expediente</MenuItem>
-                    <MenuItem value={2}>Início Intervalo</MenuItem>
-                    <MenuItem value={3}>Fim Intervalo</MenuItem>
-                    <MenuItem value={4}>Fim Expediente</MenuItem>
-                </Select>
+        <form noValidate onSubmit={handleSubmit}>
+            <Grid container spacing={2} sx={{ display: "flex", alignItems: "end" }}>
+                <Grid item xs={12} sm={12} md={4}>
+                    <label htmlFor='type'>Tipo:</label>
+                    <Select
+                        name="type"
+                        label="Tipo"
+                        labelId='select-type'
+                        value={values.type}
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={1}>Início Expediente</MenuItem>
+                        <MenuItem value={2}>Início Intervalo</MenuItem>
+                        <MenuItem value={3}>Fim Intervalo</MenuItem>
+                        <MenuItem value={4}>Fim Expediente</MenuItem>
+                    </Select>
+                </Grid>
+                <Grid item xs={12} sm={12} md={4}>
+                    <label htmlFor='time'>Hora:</label>
+                    <TextField
+                        name="time"
+                        variant="outlined"
+                        value={values.time}
+                        onChange={handleChange}
+                        placeholder="Informe a hora"
+                    />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4}>
+                    <Button
+                        sx={{ height: "55px" }}
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                    >
+                        <AddCircleOutlineIcon />
+                    </Button>
+                </Grid>
             </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-                <TextField
-                    variant="outlined"
-                    value={time}
-                />
-            </Grid>
-            <Grid item xs={12} sm={12} md={4}>
-                <Button
-                    sx={{ height: "100%" }}
-                    variant="contained"
-                    color="primary"
-                >
-                    <AddCircleOutlineIcon />
-                </Button>
-            </Grid>
-        </Grid>
+        </form>
     )
+
 }
