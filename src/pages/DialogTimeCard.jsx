5bd7ab247bb0bbb64a8905297e928c2e0fb85217
Ricardo Barros <169519@upf.br>
2022-08-23 10:41:31 -0300

    feat: created function that add new time card in row index

--- src/pages/DialogTimeCard.jsx
+++ src/pages/DialogTimeCard.jsx
@@ -1,5 +1,10 @@
 import React, { useContext } from 'react'
 import { TimeCardContext } from '../components/Context/TimeCardContext'
+import TimeCardInsertionOptions from '../components/DialogTimeCard/TimeCardInsertionOptions'
+import TimeCardGridData from '../components/DialogTimeCard/TimeCardGridData'
+import { DialogDataContext } from '../components/Context/DialogContext'
+import { TimeCardDataContext } from '../components/Context/TimeCardDataContext'
+import { getCurrentDateTime } from '../utils/date'
 import {
     Button,
     Dialog,
@@ -7,33 +12,58 @@ import {
     DialogActions,
     DialogTitle
 } from '@mui/material'
-import TimeCardInsertionOptions from '../components/DialogTimeCard/TimeCardInsertionOptions'
-import TimeCardGridData from '../components/DialogTimeCard/TimeCardGridData'
-import DialogDataProvider, { DialogDataContext } from '../components/Context/DialogContext'
+import { getNextId } from '../utils/index/gridConfig'
 
 export default function DialogTimeCard() {
 
     const { openDialog, handleClick } = useContext(TimeCardContext)
+    const { rowsDialog, setRowsDialog } = useContext(DialogDataContext)
+    const { rowsIndex, setRowsIndex } = useContext(TimeCardDataContext)
+
+    const handleSubmit = () => {
+        let newRow = [...rowsDialog]
+
+        let newTimeCard = newRow.reduce((acc, currentValue) => {
+
+            switch (currentValue.typeId) {
+                case 2:
+                    acc = { ...acc, start_interval: currentValue['time'] }
+                    break
+                case 3:
+                    acc = { ...acc, end_interval: currentValue['time'] }
+                    break
+                case 4:
+                    acc = { ...acc, end_office: currentValue['time'] }
+                    break
+                default:
+                    acc = { ...acc, start_office: currentValue['time'] }
+                    break
+            }
+
+            return acc
+        }, {})
+
+        setRowsIndex([...rowsIndex, { ...newTimeCard, id: getNextId(rowsIndex), date_time: getCurrentDateTime() }])
+        setRowsDialog([])
+        handleClick(false)
+    }
 
     return (
-        <DialogDataProvider>
-            <Dialog
-                fullWidth={true}
-                maxWidth="sm"
-                open={openDialog}
-                onClose={() => handleClick(false)}
-            >
-                <DialogTitle>Novo Ponto</DialogTitle>
-                <DialogContent>
-                    <TimeCardInsertionOptions />
-                    <TimeCardGridData />
-                </DialogContent>
-                <DialogActions>
-                    <Button variant='contained' color="error" onClick={() => handleClick(false)}>Fechar</Button>
-                    <Button variant='contained' color="success">Salvar</Button>
-                </DialogActions>
-            </Dialog>
-        </DialogDataProvider>
-
+        <Dialog
+            fullWidth={true}
+            maxWidth="sm"
+            open={openDialog}
+            onClose={() => handleClick(false)}
+        >
+            <DialogTitle>Novo Ponto</DialogTitle>
+            <DialogContent>
+                <TimeCardInsertionOptions />
+                <TimeCardGridData />
+            </DialogContent>
+            <DialogActions>
+                <Button variant='contained' color="error" onClick={() => handleClick(false)}>Fechar</Button>
+                <Button variant='contained' color="success" onClick={handleSubmit}>Salvar</Button>
+            </DialogActions>
+        </Dialog>
     )
 }
